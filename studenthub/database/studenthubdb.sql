a6aaa00b161253d472450c9fda911735124c74f3
muteebaa <muteeba.jamal02@gmail.com>
2023-02-13 15:44:08 -0700

    Added username attribute to registered-users table

--- studenthub/database/studenthubdb.sql
+++ studenthub/database/studenthubdb.sql
@@ -7,6 +7,7 @@ DROP TABLE IF EXISTS `registered-users`;
 CREATE TABLE `registered-users` (
   `email` varchar(45) NOT NULL,
   `ucid` int NOT NULL,
+  `username` varchar(45) NOT NULL,
   `password` char(40) NOT NULL,
   PRIMARY KEY (`email`,`ucid`)
 );

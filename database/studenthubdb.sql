d84d3e02f3bb8d4b1ec68adc78765ae8993f87dd
muteebaa <muteeba.jamal02@gmail.com>
2023-02-13 15:04:04 -0700

    Created database with registered-users table

---
+++ database/studenthubdb.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS STUDENTHUB;
+CREATE DATABASE STUDENTHUB; 
+USE STUDENTHUB;
+
+DROP TABLE IF EXISTS `registered-users`;
+ 
+CREATE TABLE `registered-users` (
+  `email` varchar(45) NOT NULL,
+  `ucid` int NOT NULL,
+  `password` char(40) NOT NULL,
+  PRIMARY KEY (`email`,`ucid`)
+);

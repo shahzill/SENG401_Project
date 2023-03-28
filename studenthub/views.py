6cb57515940d12902a0ee02e877f422dd812618c
muteebaa <muteeba.jamal02@gmail.com>
2023-03-28 15:45:24 -0600

    edited files for deployment

--- studenthub/views.py
+++ studenthub/views.py
@@ -7,7 +7,7 @@ from signIn import views
 
 def homepage(request):
     views.loggedIn = 0
-    return render(request, 'studenthub\main.html')
+    return render(request, 'studenthub/main.html')
 
 
 
